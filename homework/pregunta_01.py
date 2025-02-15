e25a1d64112dacbbd4dd3a07b0015f047f2c90c7
Ljm <111093017+NoGitNoLife@users.noreply.github.com>
2025-02-15 16:13:09 -0500

    Add files via upload

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,20 +1,36 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta.
-"""
-
-# pylint: disable=import-outside-toplevel
-
+import pandas as pd
 
 def pregunta_01():
-    """
-    Construya y retorne un dataframe de Pandas a partir del archivo
-    'files/input/clusters_report.txt'. Los requierimientos son los siguientes:
 
-    - El dataframe tiene la misma estructura que el archivo original.
-    - Los nombres de las columnas deben ser en minusculas, reemplazando los
-      espacios por guiones bajos.
-    - Las palabras clave deben estar separadas por coma y con un solo
-      espacio entre palabra y palabra.
+    filename = "files/input/clusters_report.txt"
+    with open(filename, 'r', encoding='utf-8') as file:
+        data_lines = file.readlines()[4:]
 
+    processed_data = []
+    temp_data = []
 
-    """
+    for line in data_lines:
+        line = line.strip()
+
+        if line:
+            elements = line.split()
+            if len(temp_data) == 0:
+                temp_data = [
+                    int(elements[0]),
+                    int(elements[1]),
+                    float(elements[2].replace(',', '.')),
+                    " ".join(elements[4:])
+                ]
+            else:
+                temp_data[3] += " " + " ".join(elements)
+        else:
+            if temp_data:
+                temp_data[3] = temp_data[3].replace('.', '')
+                processed_data.append(temp_data)
+                temp_data = []
+
+    if temp_data:
+        temp_data[3] = temp_data[3].replace('.', '')
+        processed_data.append(temp_data)
+
+    return pd.DataFrame(processed_data, columns=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
